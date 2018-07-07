90e56731f03432cccde90c5d5fbe8db06853a27c
Peter Hwu <peterbhwu@gmail.com>
2018-07-07 14:42:23 -0700

    nginx config included

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,4 @@ COPY package*.json ./
 RUN npm install
 COPY . .
 EXPOSE 1337
+CMD ["npm", "start"]
--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 let db;
-const uri = 'mongodb://52.15.108.210:27017/photos';
+const uri = 'mongodb://18.221.80.31:27017/photos';
 
 
 MongoClient.connect(uri, { poolSize: 10 }, (err, client) => {
