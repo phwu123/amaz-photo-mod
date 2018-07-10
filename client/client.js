cb5aabac166fd3027f6e632ca6232f29d3942795
Peter Hwu <peterbhwu@gmail.com>
2018-07-10 12:00:54 -0700

    client renders as standalone after ssr changes

---
+++ client/client.js
@@ -0,0 +1,9 @@
+import App from './src/components/App.jsx';
+import Photos from './src/components/Photos.jsx';
+import ThumbRender from './src/components/ThumbRender.jsx';import * as Index from './src/index.jsx';
+
+
+window.App = App;
+window.Photos = Photos;
+window.ThumbRender = ThumbRender;
+window.Index = Index;
---
+++ client/server.js
@@ -0,0 +1,11 @@
+import App from './src/components/App.jsx';
+import Photos from './src/components/Photos.jsx';
+import ThumbRender from './src/components/ThumbRender.jsx';
+import * as Index from './src/index.jsx';
+
+export {
+  App,
+  Photos,
+  ThumbRender,
+  Index
+}
